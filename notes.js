ddc0ca27e1fc5b3de1290f0b907203392cccad36
syedimthiyaz <syedaala348@gmail.com>
2024-07-18 21:35:56 +0530

    third commit

--- notes.js
+++ notes.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const userSchema = new mongoose.Schema({
   title: String,
   content: String,
-  status : String,
+  status: String,
 });
 
 module.exports = mongoose.model("notes", userSchema);
